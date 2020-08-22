3ce98a7bcc7a7a7731b2c999342812243f3262c1
yuuuko86326274 <yuko.67.53@gmail.com>
2020-08-22 16:06:46 +0900

    サイドバーとチャット画面ファイルの分割

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "messages#index"
 end
